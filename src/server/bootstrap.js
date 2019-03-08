a4cb826d851e811279327b74f03992481ac0d701
Krishna Glick <unstoppable411@gmail.com>
2019-03-08 15:06:43 -0500

    ty formatter

--- src/server/bootstrap.js
+++ src/server/bootstrap.js
@@ -2,9 +2,14 @@ require("ignore-styles");
 require("@babel/polyfill");
 
 require("@babel/register")({
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
-    presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript", "mobx"],
-    plugins: ["@babel/plugin-syntax-dynamic-import", "@loadable/babel-plugin"],
+  extensions: [".js", ".jsx", ".ts", ".tsx"],
+  presets: [
+    "@babel/preset-env",
+    "@babel/preset-react",
+    "@babel/preset-typescript",
+    "mobx"
+  ],
+  plugins: ["@babel/plugin-syntax-dynamic-import", "@loadable/babel-plugin"]
 });
 
 require("./index");
