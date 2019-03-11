fabdf7c08583ea175e35e8acbdbe8b1b64e940c3
Krishna Glick <unstoppable411@gmail.com>
2019-03-11 09:25:59 -0400

    Attempts to fix issue based on @loadable example

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const webpack = require("webpack");
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: ["@babel/polyfill", "./src/client/index.tsx"],
   module: {
     rules: [
